51c16085988b3d45ea558544edba3e6e02ff4db0
Muhammad Kurnia Affandi <affandi.kurnia@gmail.com>
2023-07-19 17:09:48 +0700

    mengenal variabel di python

--- 04 - variabel/Main.py
+++ 04 - variabel/Main.py
@@ -1,1 +1,27 @@
-print("hello world")
+# variabel adalah tempat untuk menyimpan data
+# variabel diawali dengan huruf atau underscore
+# untuk membuat variabel tidak perlu menuliskan tipe data
+
+# assignment nilai
+a = 10
+x = 5
+panjang = 1000
+
+# pemanggilan pertama
+print("Nilai a = ", a)
+print("Nilai x = ", x)
+print("Nilai panjang = ", panjang)
+
+# penamaan variabel
+nilai_y = 15 # dengan menggunakan underscore
+juta10 = 10000000 # ini boleh
+nilaiZ = 17.5 # ini boleh
+
+# pemanggilan kedua
+print("Nilai a = ", a)
+a = 7
+print("Nilai a = ", a)
+
+# assignment indirect
+b = a
+print("Nilai b = ", b)
